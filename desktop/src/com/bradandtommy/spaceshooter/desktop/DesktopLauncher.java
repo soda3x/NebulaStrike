43cc79d661c7f0ec75c2aaac24a189e3a4be75aa
Bradley Newman <bnewman227@gmail.com>
2020-05-22 10:20:45 +0930

    Initialise Project

---
+++ desktop/src/com/bradandtommy/spaceshooter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bradandtommy.spaceshooter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bradandtommy.spaceshooter.SpaceShooter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceShooter(), config);
+	}
+}
