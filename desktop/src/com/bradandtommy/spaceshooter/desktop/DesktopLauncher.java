1a2691dbf843cfa78bb972dd081647fbdeb0e054
Bradley Newman <bnewman227@gmail.com>
2020-06-06 17:19:06 +0930

    Make desktop config non-resizable

--- desktop/src/com/bradandtommy/spaceshooter/desktop/DesktopLauncher.java
+++ desktop/src/com/bradandtommy/spaceshooter/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.bradandtommy.spaceshooter.SpaceShooter;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.resizable = false;
 		new LwjglApplication(SpaceShooter.getSpaceShooterInstance(), config);
 	}
 }
